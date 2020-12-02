89f8fa3265cf5289c37c0026aba00ad9a720d4bc
Diego <diegobustos1229@gmail.com>
2020-12-01 21:45:24 -0800

    Header on FAQ

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Layout from "../../components/Layout";
 import FloatingActionButton from '../../components/FloatingActionButton';
+import Header from "../../components/Header";
+
 
 export default class FAQ extends Component {
     render() {
         return (
             <Layout>
+                <Header />
 
                 <FloatingActionButton />
 
